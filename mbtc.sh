ca20aaacf00dc7597b6cf8141ed353da49f55c0b
oth-body <54001179+oth-body@users.noreply.github.com>
2022-12-17 11:39:48 -0500

    Create mbtc.sh
    script to show the last BTC price as mBTC pulled from Bitfinex in xBar

---
+++ mbtc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Shows last BTC price formatted as mBTC(in USD) on Bitfinex BTCUSD Order Book.
+#
+# <xbar.title>mBTC price</xbar.title>
+# <xbar.version>v1.0</xbar.version>
+# <xbar.author>oth-body</xbar.author>
+# <xbar.author.github>oth-body</xbar.author.github>
+# <xbar.desc>Shows last BTC price formatted as mBTC(in USD) on Bitfinex BTCUSD Order Book.</xbar.desc>
+# <xbar.image>http://i.imgur.com/AJU4wmc.png</xbar.image>
+#
+# by oth-body
+# Based on Coinbase bitbar plugin by Mat Ryer and xbar plugin by Roberto Santacroce Martins
+echo -n "m₿TC$"; curl -s "https://api.bitfinex.com/v1/pubticker/BTCUSD" | egrep -o '"last_price":"[0-9]+(\.)?([0-9]{0,2}")?' | sed 's/"last_price"://' | sed 's/\"//g' | awk '{printf "%.2f", $1/1000}'
